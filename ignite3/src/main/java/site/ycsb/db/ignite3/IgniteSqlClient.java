31743f9b3dc44f8cb63e2ef4aebdac5ce89275dd
Ivan Artiukhov <iartukhov@gridgain.com>
2023-06-29 16:06:24 +0300

    fix IgniteSqlClient#read

--- ignite3/src/main/java/site/ycsb/db/ignite3/IgniteSqlClient.java
+++ ignite3/src/main/java/site/ycsb/db/ignite3/IgniteSqlClient.java
@@ -45,7 +45,13 @@ public class IgniteSqlClient extends IgniteAbstractClient {
   @Override
   public Status read(String table, String key, Set<String> fields, Map<String, ByteIterator> result) {
     try {
-      String qry = String.format("SELECT FROM %s WHERE %s = '%s'", table, PRIMARY_COLUMN_NAME, key);
+      // TODO: move to parent class?
+      List<String> columns = new ArrayList<>();
+      for (int i = 0; i < fieldCount; i++) {
+        columns.add(fieldPrefix + i);
+      }
+
+      String qry = String.format("SELECT * FROM %s WHERE %s = '%s'", table, PRIMARY_COLUMN_NAME, key);
 
       if (debug) {
         LOG.info(qry);
@@ -59,13 +65,15 @@ public class IgniteSqlClient extends IgniteAbstractClient {
         }
 
         if (fields.isEmpty()) {
-          for (int i = 0; i < fieldCount; i++) {
-            fields.add(fieldPrefix + i);
-          }
+          fields.addAll(columns);
         }
 
         for (String column : fields) {
-          String val = row.stringValue(column);
+          // TODO: this does not work. A bug?
+          // String val = row.stringValue(column);
+
+          // Shift to exclude the first column from the result
+          String val = row.stringValue(columns.indexOf(column) + 1);
 
           if (val != null) {
             result.put(column, new StringByteIterator(val));

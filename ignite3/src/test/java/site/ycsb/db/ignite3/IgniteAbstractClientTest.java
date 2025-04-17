b2fbf0e61c64d81b7fca35f2deb522253da5a496
Vladimir Dmitrienko <uladzimir.dzmitryienka@gmail.com>
2025-04-17 11:10:51 +0200

    Use the new CREATE ZONE syntax

--- ignite3/src/test/java/site/ycsb/db/ignite3/IgniteAbstractClientTest.java
+++ ignite3/src/test/java/site/ycsb/db/ignite3/IgniteAbstractClientTest.java
@@ -27,7 +27,7 @@ public class IgniteAbstractClientTest {
   @Test
   public void testCreateDdl() throws DBException {
     doTestCreateDdl(
-        "CREATE ZONE IF NOT EXISTS Z1 WITH STORAGE_PROFILES='default,myColumnarStore';",
+        "CREATE ZONE IF NOT EXISTS Z1 STORAGE PROFILES ['default,myColumnarStore'];",
         List.of(CREATE_TABLE_DDL +
                 " ZONE \"Z1\" STORAGE PROFILE 'default' SECONDARY ZONE \"Z1\" SECONDARY STORAGE PROFILE 'myColumnarStore'"),
         true);
